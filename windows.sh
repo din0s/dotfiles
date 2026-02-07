fd1641475e21633fb65aeba004068550d97e96d5
Dinos Papakostas <dinospk1999@gmail.com>
2026-02-07 16:48:25 +0100

    rm all old files

--- windows.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-WINDOWS_TITLE=$(grep -i 'windows' /boot/grub/grub.cfg|grep "^[^#;]"|cut -d"'" -f2) 
-sudo grub-reboot "$WINDOWS_TITLE" 
-echo "Your computer will reboot on ${WINDOWS_TITLE} in 3 seconds."
-echo "Press Ctrl+C if you want to abort!"
-sleep 3 && sudo reboot
